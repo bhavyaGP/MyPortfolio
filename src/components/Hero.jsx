0b5a19a5f2fb6803eadb4411b835c57f55ef4f92
Bhavya <bgprajapati575@gmail.com>
2024-11-09 10:59:05 +0530

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                             initial={{ opacity: 0, x: -100 }}
                             animate={{ opacity: 1, x: 0 }}
                             transition={{ duration: 1 }}
-                            className="pb-16 text-6xl font-medium tracking-tight lg:mt-16 lg:text-6xl"
+                            className="pb-16 text-4xl font-medium tracking-tight sm:text-5xl lg:mt-16 lg:text-6xl"
                         >
                             Bhavya Prajapati
                         </motion.h1>
@@ -21,7 +21,7 @@ const Hero = () => {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 0.5, duration: 1 }}
-                            className="bg-gradient-to-r from-pink-400 viaslat-500 to-violet-500 bg-clip-text text-2xl tracking-tight text-transparent"
+                            className="bg-gradient-to-r from-pink-400 via-slat-500 to-violet-500 bg-clip-text text-xl tracking-tight text-transparent sm:text-2xl"
                         >
                             Backend Developer
                         </motion.span>
@@ -29,7 +29,7 @@ const Hero = () => {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 1, duration: 1 }}
-                            className="my-2 max-w-xl py-6 font-light tracking-tighter"
+                            className="my-2 max-w-xl py-6 font-light tracking-tighter text-sm sm:text-base"
                         >
                             {HERO_CONTENT}
                         </motion.p>
@@ -44,7 +44,7 @@ const Hero = () => {
                             animate={{ opacity: 1, scale: 1 }}
                             transition={{ delay: 1.5, duration: 1 }}
                             className="rounded-full object-cover"
-                            style={{ height: 'auto', maxHeight: '450px', width: '100%', maxWidth: '450px' }}
+                            style={{ height: 'auto', maxHeight: '300px', width: '100%', maxWidth: '300px', sm: { maxHeight: '450px', maxWidth: '450px' } }}
                         />
                     </div>
                 </div>
